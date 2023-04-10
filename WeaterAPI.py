ae22676b7a68563e57c6f089d608089003c68655
Ilana Eydelman <ilanaeydelman@Ilanas-MacBook-Air-2.local>
2023-04-10 01:22:57 -0400

    created database and table, made api call, insert data into weather table

--- WeaterAPI.py
+++ WeaterAPI.py
@@ -1,30 +1,72 @@
-'''
-Plan:
-1.) request API and get a dictionary or list of 100 days 
-between months of may-august 2022
-2.) get tempeture, weather conditions: percipation, wind, etc..
-3.) put in a csv on github maybe
-4.) or json file 
-    {date:[temperature, percipatation, wind, extra]...}
-
-Functions to make:
-1.) get weather api 
-    make a request and import data into dicitonary or file
-2.) write to database 
-3.) functions to grab from past work
-    setupdatabase - hw8
-    main 
-    write_csv - project 2
-
-'''
-
 
 
 # Imports
 import os
 import requests
 import json
-import re
-import csv 
+import sqlite3
+from datetime import datetime, timedelta
 
 # Task 1:
+
+def create_database():
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+"final.database")
+    cur = conn.cursor()
+    cur.execute("DROP TABLE IF EXISTS Weather_data")
+    cur.execute('''CREATE TABLE IF NOT EXISTS weather_data (
+                 date TEXT,
+                 max_temp FLOAT,
+                 min_temp FLOAT,
+                 avg_temp FLOAT,
+                 max_wind FLOAT,
+                 precip FLOAT,
+                 condition TEXT
+                 )''')
+    conn.commit()
+    return cur,conn
+    
+
+def get_weather_api(key, location, start_date, end_date):
+    url = f'https://api.weatherapi.com/v1/history.json?key={key}&q={location}&dt={start_date}&end_dt={end_date}'
+    response = requests.get(url)
+    data = response.text
+    j_dict = json.loads(data)
+    return j_dict
+
+def weather_data_table(cur, conn, data):
+    for day in data['forecast']['forecastday']:
+        date = day['date']
+        max_temp = day['day']['maxtemp_f']
+        min_temp = day['day']['mintemp_f']
+        avg_temp = day['day']['avgtemp_f']
+        max_wind = day['day']['maxwind_mph']
+        precip = day['day']['totalprecip_in']
+        condition = day['day']['condition']['text']
+
+        # Insert the data into the database table
+        cur.execute("INSERT OR IGNORE INTO weather_data (date, max_temp, min_temp, avg_temp, max_wind, precip, condition) VALUES (?, ?, ?, ?, ?, ?, ?)", 
+            (date, max_temp, min_temp, avg_temp, max_wind, precip, condition))
+    conn.commit()
+
+
+
+def main():
+    # API key for accessing the WeatherAPI
+    key = "339838be60e74f79a30210717230904"
+
+    # Location and date range for the weather data
+    location = 'New York'
+    start_date = datetime(2022, 5, 1)
+    end_date = datetime(2022, 8, 31)
+    add_days = timedelta(days=31)
+
+    cur, conn = create_database()
+    while start_date <= end_date:
+        data = get_weather_api(key,location, start_date,end_date)
+        weather_data_table(cur, conn, data)
+        start_date += add_days
+    conn.close()
+
+if __name__ == "__main__":
+    main()
